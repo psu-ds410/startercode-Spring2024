9495ccf051c701e6a1c832adf0d6221940205fd0
Daniel Kifer <dkifer@cse.psu.edu>
2024-01-25 19:07:24 -0500

    .

---
+++ examples/mrjob/debug/wc_redin_grouped.py
@@ -0,0 +1,14 @@
+""" how to see what input the reducer will be running on """
+from mrjob.job import MRJob   
+
+class WordCount(MRJob):  # Choose a good class name
+    def mapper(self, key, line): # when reading a text file from hdfs, key is None and value is the line of text
+        words = line.split()
+        for w in words:
+            yield w, 1
+
+    def reducer(self, key, values):
+        yield key, list(values)
+
+if __name__ == '__main__':
+    WordCount.run()  # if you don't have these two lines, your code will not do anything 
