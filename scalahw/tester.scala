5007e74ee626c5660429b5d9742b0b2ed5ceb2a2
Daniel Kifer <dkifer@cse.psu.edu>
2024-02-25 13:03:48 -0500

    .

---
+++ scalahw/tester.scala
@@ -0,0 +1,29 @@
+object Tester {
+   
+    val q1answer: List[Int] = HW.q1(3)
+
+    val q2answer: Vector[Double] = HW.q2()
+
+    val q3answer_a: Double = HW.q3(List(1.0,2.0,3.0))
+    val q3answer_b: Double = HW.q3(Vector(3.0,4.0,5.0))
+
+    val q4answer_a: Double = HW.q4(List(1.0,2.0,3.0))
+    val q4answer_b: Double = HW.q4(Vector(3.0,4.0,5.0))
+
+    val q5answer_a: Double = HW.q5(List(1.0,2.0,3.0))
+    val q5answer_b: Double = HW.q5(Vector(3.0,4.0,5.0))
+
+    val q6answer_a: Double = HW.q6(List((1.0, 2.0), (2.0,3.0), (3.0, 4.0)))
+    val q6answer_b: Double = HW.q6(Vector((3.0,4.0), (5.0. 6.0)))
+
+    val q7answer_a: Double = HW.q7(List((1.0, 2.0), (2.0,3.0), (3.0, 4.0)))
+    val q7answer_b: Double = HW.q7(Vector((3.0,4.0), (5.0. 6.0)))
+
+    val q8answer: (Int, Int) = HW.q8(4)
+
+    val q9answer_a: Int = HW.q9(List(4,5,8,7))
+    val q9answer_b: Int = HW.q9(Vector(4,5,8,7))
+
+    val q10answer_a: Double = HW.q10(List(1.0, 1e100, 1.0, -1e100))
+    val q10answer_b: Double = HW.q10(Vector(1.0, 1e100, 1.0, -1e100))
+}
