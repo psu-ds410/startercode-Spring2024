7a940605fe733e3c490f917c9b2cf5cc2a106dd3
Daniel Kifer <dkifer@cse.psu.edu>
2024-04-09 17:34:42 -0400

    .

--- sparklab2/q4/orders.scala
+++ sparklab2/q4/orders.scala
@@ -2,11 +2,11 @@
 object Q4 {
 
 
-    def doOrders(input: RDD[String]): RDD[(String, Double)] = {
+    def doOrders(customers: RDD[String], orders: RDD[String], items: RDD[String]): RDD[(String, Double)] = {
 
     }
-    def getTestRDD(sc: SparkContext): RDD[String] = {
-
+    def getTestRDD(sc: SparkContext): (RDD[String], RDD[String], RDD[String]) = {
+        // return 3 rdds (customers, orders, items)
     }
     def runTest(sc: SparkContext) = {
 
