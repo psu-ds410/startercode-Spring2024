aa2a5745bafae1d5c4144f0253d9b9eacf4dfbc8
Daniel Kifer <dkifer@cse.psu.edu>
2024-04-16 16:42:17 -0400

    .

--- dflab/q4/orders.scala
+++ dflab/q4/orders.scala
@@ -1,6 +1,14 @@
 
 object Q4 {
 
+    def main(args: Array[String]) = {  // this is the entry point to our code
+        val spark = getSparkSession()
+        import spark.implicits._
+        val (c, o, i) = getDF(spark)
+        val counts = doOrders(c,o,i)
+        saveit(counts, "dflabq4")  // save the rdd to your home directory in HDFS
+    }
+
 
     def doOrders(customers: DataFrame, orders: DataFrame, items: DataFrame): DataFrame = {
 
