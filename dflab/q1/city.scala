29adb60102fd07d629432e4858e768c23cd0582c
Daniel Kifer <dkifer@cse.psu.edu>
2024-04-16 15:10:59 -0400

    .

--- dflab/q1/city.scala
+++ dflab/q1/city.scala
@@ -10,14 +10,13 @@ object Q1 {
     def main(args: Array[String]) = {  // this is the entry point to our code
         val spark = getSparkSession()
         import spark.implicits._
-        registerZipCounter(spark)
         val mydf = getDF(spark) 
         val counts = doCity(mydf) 
-        saveit(counts, "dflab2q1")  // save the rdd to your home directory in HDFS
+        saveit(counts, "dflabq1")  // save the rdd to your home directory in HDFS
     }
 
     def registerZipCounter(spark: SparkSession) = {
-        val zipCounter = udf({x:String => x.split(" ").size}) // creates the UDF that counts # of zip codes
+        val zipCounter = udf({x: String => Option(x) match {case Some(y) => y.split(" ").size; case None => 0}})
         spark.udf.register("zipCounter", zipCounter) // registers udf with the spark session
     }
 
