3a3ae629c5c33cc9eb76029b55ab2271ca7a8c3a
Daniel Kifer <dkifer@cse.psu.edu>
2024-04-16 15:58:39 -0400

    .

--- dflab/q4/orders.scala
+++ dflab/q4/orders.scala
@@ -6,6 +6,7 @@ object Q4 {
 
     }
     def getTestDF(spark: SparkSession): (DataFrame, DataFrame, DataFrame) = {
+        // don't forget the spark.implicits
         // return 3 dfs (customers, orders, items)
     }
     def runTest(spark: SparkSession) = {
